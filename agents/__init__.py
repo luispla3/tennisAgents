e0226df49e03fc0f8a224cddf38c033d3b43692e
Luis Planella Hernandez <luisplanellah@gmail.com>
2025-07-24 13:15:52 +0200

    __init__.py

---
+++ agents/__init__.py
@@ -0,0 +1,39 @@
+from .utils.agent_utils import Toolkit, create_msg_delete
+from .utils.agent_states import AgentState, RiskDebateState
+from .utils.memory import BettingMemory
+
+from .analysts.player import create_player_analyst
+from .analysts.weather import create_weather_analyst
+from .analysts.tournament import create_tournament_analyst
+from .analysts.odds import create_odds_analyst
+from .analysts.news import create_news_analyst
+from .analysts.social_media import create_social_media_analyst
+
+from .risk_mgmt.aggressive_debator import create_aggressive_debator
+from .risk_mgmt.neutral_debator import create_neutral_debator
+from .risk_mgmt.conservative_debator import create_conservative_debator
+from .risk_mgmt.expected_debator import create_expected_debator
+
+from .managers.manager import create_risk_manager
+
+from .trader.trader import create_trader
+
+__all__ = [
+    "BettingMemory",
+    "Toolkit",
+    "AgentState",
+    "RiskDebateState",
+    "create_msg_delete",
+    "create_player_analyst",
+    "create_weather_analyst",
+    "create_tournament_analyst",
+    "create_odds_analyst",
+    "create_news_analyst",
+    "create_social_media_analyst",
+    "create_aggressive_debator",
+    "create_neutral_debator",
+    "create_conservative_debator",
+    "create_expected_debator",
+    "create_risk_manager",
+    "create_trader",
+]
